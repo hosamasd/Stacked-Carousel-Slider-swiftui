72d445fff7362a2d2e53dc8603d065cced6dbc40
hosam <hosamelmalt@gmail.com>
2020-12-20 10:40:27 +0200

    Initial Commit

---
+++ Stacked Carousel Slider/Stacked_Carousel_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stacked_Carousel_SliderApp.swift
+//  Stacked Carousel Slider
+//
+//  Created by hosam on 20/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Stacked_Carousel_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
